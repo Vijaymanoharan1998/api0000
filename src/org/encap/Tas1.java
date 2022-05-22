f9b108b213743be84236b7530a9bd167faabcc79
Vijaymanoharan1998 <ramv3912@gmail.com>
2022-05-21 22:33:45 -0700

    dfgh

---
+++ src/org/encap/Tas1.java
@@ -0,0 +1,35 @@
+package org.encap;
+
+public class Tas1 {
+	private  int id;
+	private  short age;
+	private String name;
+	public Tas1(int id,short aged,String name) {
+		super();
+		this.id=id;
+		this.age=aged;
+		this.name=name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public short getAge() {
+		return age;
+	}
+	public void setAge(short age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+		
+
+}
---
+++ src/org/encap/Tas2.java
@@ -0,0 +1,22 @@
+package org.encap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tas2 {
+	public static void main(String[] args) {
+		Tas1 e = new Tas1( 10 ,23 ,"vijay");
+	
+	List<Tas1> li = new ArrayList<Tas1>();
+    li.add(e);
+
+System.out.println("id is"+li.get(0).getid());
+System.out.println("age is"+li.get(0).getAge());
+System.out.println("name is"+li.get(0).getName());
+
+System.out.println("name");
+	
+	
+
+}
+}
